46185e25a405915c34157329af203aa049b300dc
Lennart Buhl <git@hackmate.de>
2013-11-02 17:54:12 +0100

    manual header for net

--- src/net.h
+++ src/net.h
@@ -1,14 +1,6 @@
-/* This file was automatically generated.  Do not edit! */
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-int run_client(const struct prog_info *pinfo,void(*framecallback)(long));
-int run_server(const struct prog_info *pinfo);
+#include <sys/socket.h> // struct sockaddr
+
 void *get_in_addr(struct sockaddr *sa);
+int run_server(const struct prog_info *pinfo);
+int run_client(const struct prog_info *pinfo,void(*framecallback)(long));
+
