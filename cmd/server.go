800f849a7833808336bd55adf0c0b1831acc619e
Lennart Buhl <git@hackmate.de>
2016-02-06 21:47:06 +0100

    deregistering for clients

--- cmd/server.go
+++ cmd/server.go
@@ -42,9 +42,8 @@ var clientKeys []int = make([]int, 100)
 func handleConn(conn *net.TCPConn) {
 	var hs data.Handshake
 	log.Println("Got a connection!")
-	// handshake
-	dec := gob.NewDecoder(conn) // Decoder
-	dec.Decode(&hs)
+	dec := gob.NewDecoder(conn)
+	dec.Decode(&hs) // decode handshake
 	log.Println("Got client! ID:", hs.ID, "dimensions:", hs.W, hs.H)
 	clientKeys = append(clientKeys, hs.ID)
 	sort.Ints(clientKeys)
@@ -69,8 +68,23 @@ func serveClients() {
 			id, client := k, clients[k]
 			if id > 0 {
 				enc := gob.NewEncoder(client.con)
-				enc.Encode(oFrame)
+				err := enc.Encode(oFrame)
+				if err != nil {
+					// client disconnected
 
+					//log.Println("BEFORE remove: clients:", clients, " clientKeys:", clientKeys)
+
+					// delete client
+					delete(clients, client.id)
+
+					// delete client key
+					// ugly as fuck in go to remove from a slice
+					// it *should* work though
+					idInKeys := sort.SearchInts(clientKeys, client.id)
+					clientKeys = append(clientKeys[:idInKeys], clientKeys[idInKeys+1:]...)
+
+					//log.Println("AFTER remove: clients:", clients, " clientKeys:", clientKeys)
+				}
 				//log.Println("ID:", id, "Client:", client)
 			}
 		}
